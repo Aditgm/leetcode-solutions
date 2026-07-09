197918660c60abaf63e765553fecad43c7f059a9
Aditya Raj <adityagithubgm@gmail.com>
2026-07-09 21:16:13 +0530

    Time: 32 ms (90.44%) | Memory: 32.9 MB (60.50%) - LeetSync

---
+++ 732-my-calendar-iii/my-calendar-iii.cpp
@@ -0,0 +1,25 @@
+class MyCalendarThree {
+public:
+    map<int,int> mp;
+    MyCalendarThree() {
+        
+    }
+    
+    int book(int startTime, int endTime) {
+        mp[startTime]++;
+        mp[endTime]--;
+        int cur=0;
+        int maxi=0;
+        for(const auto&[time,cnt]:mp){
+            cur+=cnt;
+            maxi=max(maxi,cur);
+        }
+        return maxi;
+    }
+};
+
+/**
+ * Your MyCalendarThree object will be instantiated and called as such:
+ * MyCalendarThree* obj = new MyCalendarThree();
+ * int param_1 = obj->book(startTime,endTime);
+ */
