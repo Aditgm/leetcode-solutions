ddd804806cd3c3c7e1f13b9f81a71828440752e6
Aditya Raj <adityagithubgm@gmail.com>
2026-07-09 21:34:03 +0530

    Time: 187 ms (12.20%) | Memory: 165.1 MB (6.83%) - LeetSync

--- 732-my-calendar-iii/my-calendar-iii.cpp
+++ 732-my-calendar-iii/my-calendar-iii.cpp
@@ -1,25 +1,45 @@
-class MyCalendarThree {
+class Node {
 public:
-    map<int,int> mp;
-    MyCalendarThree() {
+    int val, lazy;
+    Node *left, *right;
+    Node() : val(0), lazy(0), left(nullptr), right(nullptr) {}
+};
+
+class MyCalendarThree {
+    Node* root;
+    
+    void update(Node* node, int s, int e, int l, int r) {
+        if (l <= s && e <= r) {
+            node->val += 1;
+            node->lazy += 1;
+            return;
+        }
         
+        int mid = s + (e - s) / 2;
+        if (!node->left) node->left = new Node();
+        if (!node->right) node->right = new Node();
+        
+        if (node->lazy != 0) {
+            node->left->val += node->lazy;
+            node->left->lazy += node->lazy;
+            node->right->val += node->lazy;
+            node->right->lazy += node->lazy;
+            node->lazy = 0;
+        }
+        
+        if (l < mid) update(node->left, s, mid, l, r);
+        if (r > mid) update(node->right, mid, e, l, r);
+        
+        node->val = max(node->left->val, node->right->val);
+    }
+
+public:
+    MyCalendarThree() {
+        root = new Node();
     }
     
     int book(int startTime, int endTime) {
-        mp[startTime]++;
-        mp[endTime]--;
-        int cur=0;
-        int maxi=0;
-        for(const auto&[time,cnt]:mp){
-            cur+=cnt;
-            maxi=max(maxi,cur);
-        }
-        return maxi;
+        update(root, 0, 1000000001, startTime, endTime);
+        return root->val;
     }
 };
-
-/**
- * Your MyCalendarThree object will be instantiated and called as such:
- * MyCalendarThree* obj = new MyCalendarThree();
- * int param_1 = obj->book(startTime,endTime);
- */
