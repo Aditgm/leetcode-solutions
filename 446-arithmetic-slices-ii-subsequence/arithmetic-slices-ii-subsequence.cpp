4ae72a87a37b74f4a7545d895396dd1bc2c2d36d
Aditya Raj <adityagithubgm@gmail.com>
2026-07-12 01:55:25 +0530

    Time: 59 ms (97.96%) | Memory: 33.8 MB (96.14%) - LeetSync

--- 446-arithmetic-slices-ii-subsequence/arithmetic-slices-ii-subsequence.cpp
+++ 446-arithmetic-slices-ii-subsequence/arithmetic-slices-ii-subsequence.cpp
@@ -1,19 +1,33 @@
 class Solution {
 public:
-    #define ll long long
     int numberOfArithmeticSlices(vector<int>& nums) {
-        int n=nums.size();
-        if(n<3) return 0;
-        ll tot=0;
-        vector<unordered_map<ll,int>> dp(n);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                ll dif=(ll)nums[j]-nums[i];
-                int cnt=dp[j].count(dif)?dp[j][dif]:0;
-                tot+=cnt;
-                dp[i][dif]+=cnt+1;
+        int n = nums.size();
+        if (n < 3) return 0;
+
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        unordered_map<long long, vector<int>> pos;
+        for (int i = 0; i < n; ++i) {
+            pos[nums[i]].push_back(i);
+        }
+
+        int total_ans = 0;
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                long long target = 2LL * nums[j] - nums[i];
+
+                if (pos.count(target)) {
+                    for (int k : pos[target]) {
+                        if (k >= j) break;
+                        dp[i][j] += dp[j][k] + 1;
+                    }
+                }
+
+                total_ans += dp[i][j];
             }
         }
-        return (int)tot;
+
+        return total_ans;
     }
 };
