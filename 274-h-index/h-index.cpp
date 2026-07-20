bc40260eead659cfaab55273a74e76a3988de1de
Aditya Raj <adityagithubgm@gmail.com>
2026-07-20 17:17:46 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (5.72%) - LeetSync

--- 274-h-index/h-index.cpp
+++ 274-h-index/h-index.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
+        sort(citations.rbegin(),citations.rend());
+        int h=0;
         int n=citations.size();
-        vector<int> cnt(n+1,0);
-        for(int c:citations){
-           if(c>=n){
-            cnt[n]++;
-           } 
-           else cnt[c]++;
+        while(h<n and citations[h]>h){
+            h++;
         }
-        int tot=0;
-        for(int i=n;i>=0;i--){
-            tot+=cnt[i];
-            if(tot>=i) return i;
-        }
-        return 0;
+        return h;
     }
 };
