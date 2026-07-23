edf57a97c91ce3fedb3ec8d98c468ec65ebd3ce0
Aditya Raj <adityagithubgm@gmail.com>
2026-07-23 22:59:35 +0530

    Time: 3 ms (12.76%) | Memory: 9.4 MB (5.61%) - LeetSync

---
+++ 591-tag-validator/tag-validator.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isValid(string code) {
+        stack<string> st;
+        int i=0;
+        int n=code.size();
+        while(i<n){
+            if(i>0 and st.empty()) return 0;
+            if(code.substr(i,9)=="<![CDATA["){
+                int j=code.find("]]>",i+9);
+                if(j==string::npos) return 0;
+                i=j+3;
+            }
+            else if(code.substr(i,2)=="</"){
+                int j=code.find('>',i+2);
+                if(j==string::npos) return 0;
+                string tag=code.substr(i+2,j-i-2);
+                if(st.empty() or st.top()!=tag) return 0;
+                st.pop();
+                i=j+1;
+            }
+            else if(code.substr(i,1)=="<"){
+                int j=code.find('>',i+1);
+                if(j==string::npos) return 0;
+                string tag=code.substr(i+1,j-i-1);
+                if(tag.length()<1 or tag.length()>9) return 0;
+                for(char c:tag){
+                    if(!isupper(c)) return 0;
+                }
+                st.push(tag);
+                i=j+1;
+            }
+            else i++;
+        }
+        return st.empty();
+    }
+};
