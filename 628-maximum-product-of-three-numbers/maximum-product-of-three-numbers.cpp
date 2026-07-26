b682fe1bc018381efd0b09eac0d5a09fcbe52149
Aditya Raj <adityagithubgm@gmail.com>
2026-07-26 22:22:10 +0530

    Time: 4 ms (60.10%) | Memory: 31.5 MB (46.18%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
@@ -6,23 +6,19 @@ public:
         priority_queue<int> pq2;
         priority_queue<int,vector<int>,greater<>> pq;
         for(int i=0;i<n;i++){
-            if(pq.size()<=2){
+            if(pq.size()<3){
                 pq.push(nums[i]);
             }
-            else{
-                if(nums[i]>pq.top()){
+            else if(nums[i]>pq.top()){
                     pq.pop();
                     pq.push(nums[i]);
-                }
             }
-            if(pq2.size()<=1){
+            if(pq2.size()<2){
                pq2.push(nums[i]);
             }
-            else{
-                if(pq2.top()>nums[i]){
+            else if(pq2.top()>nums[i]){
                    pq2.pop();
                 pq2.push(nums[i]);
-                }
             }
         }
         ll nums1=1,nums2=1,x=0;
