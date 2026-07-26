0f345fd6b65ceec02170d9516315f0b55b0f8b0c
Aditya Raj <adityagithubgm@gmail.com>
2026-07-26 23:13:36 +0530

    Time: 64 ms (55.77%) | Memory: 14.7 MB (50.79%) - LeetSync

--- 546-remove-boxes/remove-boxes.cpp
+++ 546-remove-boxes/remove-boxes.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-    int dp[101][101][101];
-    int rec(const vector<int>& boxes,int i,int j,int k){
-        if(i>j) return 0;
-        if(dp[i][j][k]!=0) return dp[i][j][k];
-        int ogi=i;
-        int ogk=k;
-        while(i+1<=j and boxes[i+1]==boxes[i]){
-            i++;k++;
-        }
-        int res=(k+1)*(k+1)+rec(boxes,i+1,j,0);
-        for(int m=i+1;m<=j;m++){
-            if(boxes[m]==boxes[i]){
-                int score=rec(boxes,i+1,m-1,0)+rec(boxes,m,j,k+1);
-                res=max(res,score);
+    int removeBoxes(vector<int>& boxes) {
+        int n=boxes.size();
+        int dp[n+1][n+1][n+1];
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=n;j++){
+               for(int k=0;k<=n;k++){
+                 dp[i][j][k]=-1;
+               }
             }
         }
-        return dp[ogi][j][ogk]=res;
-    }
-    int removeBoxes(vector<int>& boxes) {
-        for(int i=0;i<100;i++){
-            for(int j=0;j<100;j++){
-                for(int k=0;k<100;k++){
-                    dp[i][j][k]=0;
+        auto rec=[&](int i,int j,int k,auto &&self)->int{
+            if(i>j) return 0;
+            while(i<j and boxes[j-1]==boxes[j]){
+                j--;
+                k++;
+            }
+            int &ans=dp[i][j][k];
+            if(ans!=-1) return ans;
+            ans=self(i,j-1,0,self)+(k+1)*(k+1);
+            for(int l=i;l<j;l++){
+                if(boxes[l]==boxes[j]){
+                    ans=max(ans,self(i,l,k+1,self)+self(l+1,j-1,0,self));
                 }
             }
-        }
-        return rec(boxes,0,boxes.size()-1,0);
+            return dp[i][j][k]=ans;
+        };
+        return rec(0,n-1,0,rec);
     }
 };
