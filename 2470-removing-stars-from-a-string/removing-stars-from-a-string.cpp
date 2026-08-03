bfab5e0788c3e4d150926773731444117018843b
Aditya Raj <adityagithubgm@gmail.com>
2026-08-04 05:03:44 +0530

    Time: 27 ms (19.20%) | Memory: 28.4 MB (78.20%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -2,17 +2,14 @@ class Solution {
 public:
     string removeStars(string s) {
         int n=s.length();
-        vector<int>v;
-        vector<bool>t(n,1);
+        string ans="";
         for(int i=0;i<n;i++){
             if(s[i]=='*'){
-                t[i]=0;
-                t[v.back()]=0;v.pop_back();
+                ans.pop_back();
             }
-            else v.push_back(i);
+            else ans+=s[i];
         }
-        string ans="";
-        for(int i=0;i<n;i++)if(t[i])ans+=s[i];
+        
         return ans;
     }
 };
