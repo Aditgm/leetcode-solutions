adf4a781d7322a64289a6b329844029e3795a27a
Aditya Raj <adityagithubgm@gmail.com>
2026-08-04 05:08:08 +0530

    Time: 7 ms (96.76%) | Memory: 29.6 MB (47.24%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
-    string removeStars(string s) {
-        int n=s.length();
-        string ans="";
-        for(int i=0;i<n;i++){
-            if(s[i]=='*'){
-                ans.pop_back();
-            }
-            else ans+=s[i];
+    string removeStars(string s){
+        vector<char> st;
+        for(auto c:s){
+            if(c=='*'){if(!st.empty()) st.pop_back();}
+            else st.push_back(c);
         }
-        
+        string ans(st.begin(),st.end());
         return ans;
     }
 };
